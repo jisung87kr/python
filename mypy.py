c5ce04a15dc37ed9b59933080bcb31c9ee2d5830
zs-home <jisung87kr@gamil.com>
2019-02-12 01:04:42 +0900

    web crawling test

--- mypy.py
+++ mypy.py
@@ -1,3 +1,19 @@
 import requests
-r = requests.get('https://github.com')
-print(r.text)
+from bs4 import BeautifulSoup as bs
+
+r = requests.get('http://119.205.79.142/wordpress/')
+soup = bs(r.text, 'html.parser')
+main = soup.find('main', id='main')
+posts = main.find_all('article', class_='post')
+
+data = []
+for post in posts:
+    tp = [
+        {
+            'title' : post.find('h3', class_='entry-title').find('a').string,
+            'link' : post.find('h3', class_='entry-title').find('a').get('href')
+        }
+    ]
+    data.append(tp)
+    
+print(data)
